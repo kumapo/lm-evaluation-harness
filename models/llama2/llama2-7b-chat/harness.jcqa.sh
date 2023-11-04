d25fec09597c687f032990326a71d93440dcdfc8
kumapo <kumapo@users.noreply.github.com>
2023-11-04 17:59:26 +0900

    wont need harness.jcqa.sh

--- models/llama2/llama2-7b-chat/harness.jcqa.sh
+++
@@ -1,3 +0,0 @@
-MODEL_ARGS="pretrained=meta-llama/Llama-2-7b-chat-hf,use_accelerate=True,dtype=auto"
-TASK="jcommonsenseqa-1.1-0.3"
-python main.py --model hf-causal-experimental --model_args $MODEL_ARGS --tasks $TASK --num_fewshot "3" --device "cuda" --output_path "models/llama2/llama2-7b-chat/result.jcqa.json" --batch_size 2
--- models/rinna/rinna-bilingual-gpt-neox-4b-instruction-ppo/harness.jcqa.sh
+++
@@ -1,3 +0,0 @@
-MODEL_ARGS="pretrained=rinna/bilingual-gpt-neox-4b-instruction-ppo,use_fast=False,device_map=auto,torch_dtype=auto"
-TASK="jcommonsenseqa-1.1-0.5"
-python main.py --model hf-causal --model_args $MODEL_ARGS --tasks $TASK --num_fewshot "3" --device "cuda" --output_path "models/rinna/rinna-bilingual-gpt-neox-4b/result.jcqa.json"
